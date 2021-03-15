e42e899378596a6d1facc1e8f077960f0fd9cfc5
JadielsonSantos <78828355+JadielsonSantos@users.noreply.github.com>
2021-03-15 20:37:26 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,5 +5,6 @@ function gera() {
 	var pronto = mensagens.value;
 	var ulrpronta = pronto.replace(/ /g, '%20')
 	var api =  `https://api.whatsapp.com/send?phone=55${numero.value}&text=${ulrpronta}`;
+	url.href = api
 	url.innerHTML = api;
 }
